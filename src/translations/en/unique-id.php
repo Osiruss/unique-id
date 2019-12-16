b4c761b1bb6e86aac6d7b8da36a31aee067057e9
Russ <russellkitchen@live.com>
2019-12-16 08:44:47 +0000

    Init

---
+++ src/translations/en/unique-id.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Unique ID plugin for Craft CMS 3.x
+ *
+ * Set a unique ID
+ *
+ * @link      https://github.com/Osiruss
+ * @copyright Copyright (c) 2019 osiruss
+ */
+
+/**
+ * Unique ID en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('unique-id', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    osiruss
+ * @package   UniqueId
+ * @since     1.0.0
+ */
+return [
+    'Unique ID plugin loaded' => 'Unique ID plugin loaded',
+];
